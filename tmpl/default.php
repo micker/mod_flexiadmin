391a920e6db6851e93fe742a879f9a69bac44e81
Ignacio Correia <ignacio.correia@algardata.pt>
2014-09-11 19:01:51 +0100

    Small Misspell
    Module instead of Moduk

--- tmpl/default.php
+++ tmpl/default.php
@@ -8,7 +8,7 @@
 
 * special thanks to ggppdk and emmanuel dannan for flexicontent
 
-* FLEXIadmin moduke is distributed in the hope that it will be useful,
+* FLEXIadmin module is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
 * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 * GNU General Public License for more details. 
