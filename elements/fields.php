be088e9e92c430aeb588ef2e4387482c73ef3a2e
Berges Yannick <micker@free.fr>
2017-03-02 22:13:26 +0100

    Update display field in module
    Now you can use all field in list display

---
+++ elements/fields.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @version 1.5 stable $Id: separator.php 1904 2014-05-20 12:21:09Z ggppdk $
+ * @package Joomla
+ * @subpackage FLEXIcontent
+ * @copyright (C) 2009 Emmanuel Danan - www.vistamedia.fr
+ * @license GNU/GPL v2
+ * 
+ * FLEXIcontent is a derivative work of the excellent QuickFAQ component
+ * @copyright (C) 2008 Christoph Lukes
+ * see www.schlu.net for more information
+ *
+ * FLEXIcontent is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+// Check to ensure this file is included in Joomla!
+defined('_JEXEC') or die('Restricted access');
+
+jimport( 'joomla.application.component.controller' );
+// Check if component is installed
+if ( !JComponentHelper::isEnabled( 'com_flexicontent', true) ) {
+   echo 'This modules requires component FLEXIcontent!';
+   return;
+}
+	//Get FC Types of Content Helper
+	require_once (JPATH_ADMINISTRATOR.DS.'components'.DS.'com_flexicontent'.DS.'elements'.DS.'fields.php');
+?>
