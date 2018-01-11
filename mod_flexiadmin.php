9c28c5aa58dd5763c8e2da842071a3dc2e8c4bb3
Berges Yannick <micker@free.fr>
2018-01-11 10:26:05 +0100

    Update mod_flexiadmin.php

--- mod_flexiadmin.php
+++ mod_flexiadmin.php
@@ -32,6 +32,7 @@ $listDraft        = modFlexiadminHelper::getDraft($params);
 $listTrashed      = modFlexiadminHelper::getTrashed($params);
 $listUseritem     = modFlexiadminHelper::getUseritem($params);
 $listCustomlist  = modFlexiadminHelper::getCustomlist($params);
+print_r ($listCustomlist);
 /*$listCustomlist2  = modFlexiadminHelper::getCustomlist2($params);
 $listCustomlist3  = modFlexiadminHelper::getCustomlist3($params);
 $listCustomlist4  = modFlexiadminHelper::getCustomlist4($params);
